dd9c21cce9331215111d5ed922c73a7fad140ee9
Yash Agarwal <yash23.agarwal@gmail.com>
2020-08-03 01:00:52 +0530

    resolved bugs in scapy packet sniffing

--- watchdog/log_network.py
+++ watchdog/log_network.py
@@ -17,47 +17,53 @@ def getPacketDirection(packet):
         return "outgoing"
 
 
-def getTcpData(tcp_packet):
-    packet_dump = tcp_packet.show(dump=True)
-    packet_dump_line_list = packet_dump.split('\n')[1:12]
+def getTcpData(packet):
     packet_dump_dict = {}
-    for line in packet_dump_line_list:
-        key, value = line.split('=')
-        packet_dump_dict[key.strip()] = value.strip()
+    packet_dump_dict['sport'] = packet[TCP].sport
+    packet_dump_dict['dport'] = packet[TCP].dport
+    packet_dump_dict['seq'] = packet[TCP].seq
+    packet_dump_dict['ack'] = packet[TCP].ack
+    packet_dump_dict['dataofs'] = packet[TCP].dataofs
+    packet_dump_dict['reserved'] = packet[TCP].reserved
+    packet_dump_dict['window'] = packet[TCP].window
+    packet_dump_dict['chksum'] = packet[TCP].chksum
+    packet_dump_dict['urgptr'] = packet[TCP].urgptr
     return packet_dump_dict
 
 
-def getIpData(ip_packet):
-    packet_dump = ip_packet.show(dump=True)
-    packet_dump_line_list = packet_dump.split('\n')[1:13]
+def getIpData(packet):
     packet_dump_dict = {}
-    for line in packet_dump_line_list:
-        key, value = line.split('=')
-        packet_dump_dict[key.strip()] = value.strip()
+    packet_dump_dict['version'] = packet[IP].version
+    packet_dump_dict['ihl'] = packet[IP].ihl
+    packet_dump_dict['tos'] = packet[IP].tos
+    packet_dump_dict['len'] = packet[IP].len
+    packet_dump_dict['id'] = packet[IP].id
+    packet_dump_dict['frag'] = packet[IP].frag
+    packet_dump_dict['ttl'] = packet[IP].ttl
+    packet_dump_dict['chksum'] = packet[IP].chksum
+    packet_dump_dict['src'] = packet[IP].src
+    packet_dump_dict['dst'] = packet[IP].dst
     return packet_dump_dict
 
-def getUdpData(udp_packet):
-    packet_dump = udp_packet.show(dump=True)
-    packet_dump_line_list = packet_dump.split('\n')[1:5]
+def getUdpData(packet):
     packet_dump_dict = {}
-    for line in packet_dump_line_list:
-        key, value = line.split('=')
-        packet_dump_dict[key.strip()] = value.strip()
+    packet_dump_dict['sport'] = packet[UDP].sport
+    packet_dump_dict['dport'] = packet[UDP].dport
+    packet_dump_dict['chksum'] = packet[UDP].chksum
+    packet_dump_dict['len'] = packet[UDP].len
     return packet_dump_dict
+
 
 def log_packet(packet):
     packet_dict = {}
     if IP in packet:
-        ip = IP(packet)
-        packet_dict.update(getIpData(ip))
+        packet_dict.update(getIpData(packet))
 
     if TCP in packet:
-        tcp = TCP(packet)
-        packet_dict.update(getTcpData(tcp))
+        packet_dict.update(getTcpData(packet))
 
     if UDP in packet:
-        udp = UDP(packet)
-        packet_dict.update(getUdpData(udp))
+        packet_dict.update(getUdpData(packet))
 
     packet_dict["timestamp"] = str(datetime.fromtimestamp(time.time()))
     packet_dict["server"] = str(socket.gethostname())
